47681c7a1b2e8702ee38af95fdec6f3fd242f084
Szymon Misztal <sz.misztal@gmail.com>
2023-04-29 12:41:39 +0200

    modify tables, fix bug book > author

--- tables.py
+++ tables.py
@@ -1,20 +1,22 @@
-create_books_sql = """
-CREATE TABLE IF NOT EXISTS books (
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    year_of_publication INTEGER NOT NULL,
-    genre TEXT
-);
-"""
-
 create_authors_sql = """
 CREATE TABLE IF NOT EXISTS authors (
     id INTEGER PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     birth_date DATE,
-    death_date DATE,
-    book_id INTEGER NOT NULL,
-    FOREIGN KEY (book_id) REFERENCES books (id)
+    death_date DATE
 );
 """
+
+create_books_sql = """
+CREATE TABLE IF NOT EXISTS books (
+    id INTEGER PRIMARY KEY,
+    author_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    year_of_publication INTEGER NOT NULL,
+    genre TEXT,
+    short_description TEXT,
+    FOREIGN KEY (author_id) REFERENCES authors (id)
+);
+"""
+
