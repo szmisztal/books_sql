4ca80d39ccb82a529acde59788d9be8e6b374e18
Szymon Misztal <sz.misztal@gmail.com>
2023-04-30 10:54:32 +0200

    add select_all and select_where functions to search values in database

--- main.py
+++ main.py
@@ -25,19 +25,46 @@ def add_book(conn, book):
     conn.commit()
     return cur.lastrowid
 
+def select_all(conn, table):
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM {table}")
+    rows = cur.fetchall()
+    return rows
+
+def select_where(conn, table, query):
+   cur = conn.cursor()
+   values = tuple(query.values())
+   conditions = ' AND '.join(f'{k} = ?' for k in query.keys())
+   cur.execute(f"SELECT * FROM {table} WHERE {conditions}", values)
+   rows = cur.fetchall()
+   return rows
+
 if __name__ == '__main__':
     db_file = "books_db"
     conn = create_connection(db_file)
+    cur = conn.cursor()
     if conn is not None:
         execute_sql(conn, tables.create_books_sql)
         execute_sql(conn, tables.create_authors_sql)
 
     author = ("Andrzej", "Sapkowski", "1948-06-21", None)
     author_id = add_author(conn, author)
+    author_2 = ("J.R.R", "Tolkien", "1892-01-03", None)
+    author_2_id = add_author(conn, author_2)
     book = (author_id, "Ostatnie życzenie", 2014, "Fantasy", "Ciachanie potworów")
+    add_book(conn, book)
     book_2 = (author_id, "Miecz przeznaczenia", 2014, "Fantasy", "Więcej ciachania potworów")
-    book_id = add_book(conn, book)
-    book_2_id = add_book(conn, book_2)
+    add_book(conn, book_2)
+    book_3 = (author_2_id, "Drużyna Pierścienia", 1954, "Dark Fantasy", "Wycieczka do Mordoru")
+    add_book(conn, book_3)
+    book_4 = (author_2_id, "Dwie Wieże", 1954, "Dark Fantasy", "Wycieczki do Mordoru ciąg dalszy")
+    add_book(conn, book_4)
+    book_5 = (author_2_id, "Powrót Króla", 1955, "Dark Fantasy", "Koniec wycieczki")
+    add_book(conn, book_5)
+    conn.commit()
+    print(select_all(conn, "books"))
+    print(select_where(conn, "authors", {"first_name": "Andrzej"}))
+    print(select_where(conn, "books", {"genre": "Fantasy"}))
     conn.close()
 
 
